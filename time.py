37a44388d3e353580d48333208f7f902c71e3f48
brandon <bjd2385.linux@gmail.com>
2018-09-09 16:12:58 -0400

    time.py

--- time.py
+++ time.py
@@ -96,7 +96,7 @@ def decodeRetention(agent: str, offsite: bool =False) -> List[int]:
     with open(KEYS + agent + (OFFSITE_RETENTION if offsite else LOCAL_RETENTION))\
             as cryptic_policy:
          policy = cryptic_policy.readline().split(':')
-         list(map(lambda hours: int(hours) // 24
+         intra, list(map(lambda hours: int(hours) // 24))
 
 
 def main(arguments: argparse.Namespace) -> None:
