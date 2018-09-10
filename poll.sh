620a958be8ee1b126386612922a059fdc95604f0
brandon <bjd2385.linux@gmail.com>
2018-09-10 18:00:47 -0400

    time.py

--- poll.sh
+++ poll.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+while true; do curl -H 'Authorization: token
+22339bcf02f223f756bfd1f58ff07a609cd52367' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/bjd2385/error_codes/contents/time.py &>/dev/null; sleep 5; done &
